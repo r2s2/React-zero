9ac4f57178fc23a81f8d5eb64b299832296976ad
r2s2 <rodrigo.sacramento@gmail.com>
2024-01-04 21:22:13 -0300

    4/1/24

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,7 +1,22 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
+import logo from '../../logo.svg';
+
 
 const Formulario = () => {
+
+    
+const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+]
 
     return (
         <section className="formulario">
@@ -10,6 +25,8 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite o seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label = "Time" itens = {times}/>
+               <Botao> Criar card </Botao>
                 
             </form>
 
